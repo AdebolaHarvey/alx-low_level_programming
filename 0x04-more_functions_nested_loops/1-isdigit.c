9bbeee3f4fe2212b37d4054f0e333b8e07038902
Oyebanjo Adebola .O <oyebanjoadebola8@gmail.com>
2023-10-12 09:06:24 -0700

    A function that checks for a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit - Checks for digit.
+ * @c: The digit ti be checked.
+ * Return: 1 as digit or 0 otherwise.
+ */
+
+int _isdigit(int c)
+{
+
+	if (c >= 48 && c <= 57)
+	{
+	return (1);
+	}
+	return (0);
+
+}
