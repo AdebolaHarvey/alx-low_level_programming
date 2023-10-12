91c4159388d22865061b88ce5f125faab9a4bb5c
Oyebanjo Adebola .O <oyebanjoadebola8@gmail.com>
2023-10-12 09:46:47 -0700

    A function that prints the numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints the num from 0 to 9.
+ *
+ * Return: Void.
+ */
+
+void print_numbers(void)
+
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+	_putchar(a + '0');
+	}
+	_putchar('\n');
+}
