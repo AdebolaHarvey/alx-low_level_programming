8cb326de061aa65e206cb8f0254cae3b476fde82
Oyebanjo Adebola .O <oyebanjoadebola8@gmail.com>
2023-10-18 11:14:03 -0700

    A function that copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,25 +1,30 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * _strncpy - Copies a string.
- * @dest: The destination string value.
- * @src: The source string value
- * @n: Numbervof bytes
- * Return: The result of the string value dest.
+ * _strncpy - Copies at most an inputted number
+ * of bytes from string src into dest.
+ * @dest: The buffer storing the string copy.
+ * @src: The source string.
+ * @n: The maximum number of bytes to copied from src.
+ * Return: A pointer to the resulting string dest.
+ *
  */
 
 char *_strncpy(char *dest, char *src, int n)
 
 {
 
-	int i = 0, x = 0;
+	int index = 0, src_len = 0;
 
-	while (src[i++])
-		x++;
-	for (i = 0; src[i] && i < n; i++)
-		dest[x++] = src[i];
-	for (i = x; i < n; i++)
-		dest[i++] = '\0';
+	while (src[index++])
+	src_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+	dest[index] = src[index];
+
+	for (index = src_len; index < n; index++)
+	dest[index] = '\0';
+
 	return (dest);
+
 }
