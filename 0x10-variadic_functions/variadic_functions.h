848de8114e5a9e10d8f3a2a02bca3f0ed769281c
Oyebanjo Adebola .O <oyebanjoadebola8@gmail.com>
2023-11-09 06:38:52 -0800

    Variadic functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,7 +7,20 @@ int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
+void print_all(const char * const format, ...);	
+void print_the_char(char *separator, va_list args);
+void print_the_integer(char *separator, va_list args);
+void print_the_float(char *separator, va_list args);
+void print_the_char_ptr(char *separator, va_list args);
 
-
+/**
+ * struct format_types - Struct format_types
+ * @identifier: The conversion specifier
+ * @f: The function pointer
+ */
+typedef struct format_types
+{
+	char *identifier;
+	void (*f)(char *separator, va_list args);
+} form_dngz;
 #endif /* VARIADIC_FUNCTIONS_H */
