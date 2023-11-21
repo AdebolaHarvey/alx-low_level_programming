0f33e214851afbf791420d853ea650c7b94f50c4
Oyebanjo Adebola .O <oyebanjoadebola8@gmail.com>
2023-11-21 09:58:35 -0800

    A function that deletes the node at index of a linked list

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -26,7 +26,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	while (x < index - 1)
 	{
-		if (element == NULL || !(element->next))
+		if (!element || !(element->next))
 			return (-1);
 		element = element->next;
 		x++;
